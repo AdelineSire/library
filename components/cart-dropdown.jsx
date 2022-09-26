2259faa0a8325a1a1b8064fb764463f4b1a41d92
AdelineSire <65898303+AdelineSire@users.noreply.github.com>
2022-09-26 17:29:50 +0200

    Add toggle to open/close the cart drop-down and cartCount context

--- components/cart-dropdown.jsx
+++ components/cart-dropdown.jsx
@@ -1,27 +1,37 @@
+import { useContext } from 'react';
+
 import ShoppingBagIcon from './icons/shopping-bag-icon';
 
+import { CartContext } from '../contexts/cart.context';
+
 const CartDropdown = () => {
+	const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
+
+	const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+
 	return (
-		<div className='dropdown dropdown-end'>
+		<div onClick={toggleIsCartOpen} className='dropdown dropdown-end'>
 			<label tabIndex={0} className='btn btn-ghost btn-circle'>
 				<div className='indicator'>
 					<ShoppingBagIcon />
-					<span className='badge badge-sm indicator-item'>0</span>
+					<span className='badge badge-sm indicator-item'>{cartCount}</span>
 				</div>
 			</label>
-			<div
-				tabIndex={0}
-				className='mt-3 shadow card card-compact dropdown-content w-52 bg-base-100'
-			>
-				<div className='card-body'>
-					<span className='text-lg font-bold'>0 Articles</span>
-					<div className='card-actions'>
-						<button className='btn btn-primary btn-block'>
-							Voir le panier
-						</button>
+			{isCartOpen && (
+				<div
+					tabIndex={0}
+					className='mt-3 shadow card card-compact dropdown-content w-52 bg-base-100'
+				>
+					<div className='card-body'>
+						<span className='text-lg font-bold'>{cartCount} Articles</span>
+						<div className='card-actions'>
+							<button className='btn btn-primary btn-block'>
+								Voir le panier
+							</button>
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
