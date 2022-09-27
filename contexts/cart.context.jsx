aa40ffa0eae43dd6d9c82beeac71e31cd09a1f96
AdelineSire <65898303+AdelineSire@users.noreply.github.com>
2022-09-27 16:09:39 +0200

    Add cart sum

--- contexts/cart.context.jsx
+++ contexts/cart.context.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { addCartItem } from '../lib/addCartItem';
+import addCartItem from '../lib/addCartItem';
+import calculateCartSum from '../lib/calculateCartSum';
 
 export const CartContext = createContext({
 	isCartOpen: false,
@@ -8,19 +9,29 @@ export const CartContext = createContext({
 	cartItems: [],
 	addItemToCart: () => {},
 	cartCount: 0,
+	cartSum: 0,
 });
 
 export const CartProvider = ({ children }) => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
 	const [cartCount, setCartCount] = useState(0);
+	const [cartSum, setCartSum] = useState(0);
 
+	// Each time the cartItems array changes
 	useEffect(() => {
+		// Calculate the new amount of items in the cart
 		const newCartCount = cartItems.reduce(
 			(total, cartItem) => total + cartItem.quantity,
 			0
 		);
 		setCartCount(newCartCount);
+
+		// If the cart is not empty
+		if (newCartCount !== 0) {
+			const newSum = calculateCartSum(cartItems);
+			setCartSum(newSum);
+		}
 	}, [cartItems]);
 
 	const addItemToCart = (productToAdd) => {
@@ -34,6 +45,7 @@ export const CartProvider = ({ children }) => {
 		addItemToCart,
 		cartItems,
 		cartCount,
+		cartSum,
 	};
 
 	return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
